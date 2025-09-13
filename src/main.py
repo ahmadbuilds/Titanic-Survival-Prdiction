e6384d9b62f616aa7d60786ffb186ae0ae12a7b4
Ahmad <crisitiano678@gmail.com>
2025-09-13 12:50:01 +0500

    Converting Model to API for returning response to the frontend Completed

---
+++ src/main.py
@@ -0,0 +1,57 @@
+import joblib
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from typing import Optional
+from pydantic import BaseModel
+
+app=FastAPI()
+
+logistic_regression_model=joblib.load("../models/logistic_regression.joblib")
+random_forest_model=joblib.load("../models/random_forest.joblib")
+model_ensemble=joblib.load("../models/model_ensemble.joblib")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+class PredictionInput(BaseModel):
+    Pclass: int
+    Sex: str
+    Age: int
+    Fare: float
+    Embarked: str
+    FamilySize: int
+    isAlone: bool
+    Title: Optional[str] = "unknown"
+    TicketPrefix: Optional[str] = "unknown"
+    CabinLetter: Optional[str] = "unknown"
+
+
+@app.get("/Prediction")
+async def get_prediction(input_data: PredictionInput):
+    data_dict=input_data.dict()
+
+    if data_dict["Title"]=="unknown":
+        if data_dict["Sex"]=="male":
+            data_dict["Title"]="Mr"
+        elif data_dict["Sex"]=="female":
+            if data_dict["FamilySize"]>1:
+                data_dict["Title"]="Mrs"
+            else:
+                data_dict["Title"]="Miss"
+    
+    x_input=pd.DataFrame([data_dict])
+
+    prediction_logistic=logistic_regression_model.predict(x_input)
+    prediction_random_forest=random_forest_model.predict(x_input)
+    prediction_ensemble=model_ensemble.predict(x_input)
+
+    return {
+        "Logistic Regression Prediction": int(prediction_logistic[0]),
+        "Random Forest Prediction": int(prediction_random_forest[0]),
+        "Ensemble Model Prediction": int(prediction_ensemble[0]),
+    }
